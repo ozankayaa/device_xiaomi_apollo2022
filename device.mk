d8dbceffcc25dbd0374b522e55afbcfc08a5b718
tarsin <yuanqingxiang233@163.com>
2022-08-19 23:10:55 +0200

    apollo: Add shim for missing symbol in com.mi.node.watermark.so

--- device.mk
+++ device.mk
@@ -26,6 +26,10 @@ PRODUCT_COPY_FILES += \
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
 
+# Shims
+PRODUCT_PACKAGES += \
+    lib-watermarkshim
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
